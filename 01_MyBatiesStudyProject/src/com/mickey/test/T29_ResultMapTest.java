f09df4a5f447848820f1f02900e4e7282229ea22
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-01-29 00:12:55 +0800

    update for class 30

--- 01_MyBatiesStudyProject/src/com/mickey/test/T29_ResultMapTest.java
+++ 01_MyBatiesStudyProject/src/com/mickey/test/T29_ResultMapTest.java
@@ -2,18 +2,25 @@ package com.mickey.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.mickey.pojo.T27_Student;
+
 public class T29_ResultMapTest {
 	public static void main(String[] args) throws IOException {
 		InputStream is = Resources.getResourceAsStream("myBaties.xml");
 		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
 		SqlSession session = factory.openSession();
-		session.selectList("com.mickey.mapper.T29_ResultMapTestMapper.selectAll");
+//		session.selectList("com.mickey.mapper.T29_ResultMapTestMapper.selectAll").stream()
+//				.forEach(t -> System.out.println(t.toString()));
+
+		List<T27_Student> students = session.selectList("com.mickey.mapper.T29_ResultMapTestMapper.selectAllStu");
+		students.stream().forEach(s->System.out.println(s.toString()));
 
 	}
 }
