e719a9710f1ad1019a2c2b16748b4bfd59e060f5
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-02-03 16:00:08 +0800

    update for class 24

--- 11_SprintStudyProject/src/com/mickey/test/T23_AutoImportTest.java
+++ 11_SprintStudyProject/src/com/mickey/test/T23_AutoImportTest.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.mickey.pojo.T23_Person;
+import com.mickey.pojo.T24_User;
 
 public class T23_AutoImportTest {
 
@@ -11,6 +12,10 @@ public class T23_AutoImportTest {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		T23_Person person = ac.getBean("t23_Person", T23_Person.class);
 		System.out.println(person.toString());
+		
+		//測試class 24，注入.properties文件內容
+		T24_User user = ac.getBean("t24_User", T24_User.class);
+		System.out.println(user.toString());
 	}
 
 }
